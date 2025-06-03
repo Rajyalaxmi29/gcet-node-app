99889e22cc29aa326c3885c9e40b243ce76e4e6d
rajyalaxmi29 <rajyalaxmikunchala06@gmail.com>
2025-06-03 11:22:22 +0530

    update

--- index.js
+++ index.js
@@ -11,8 +11,20 @@ app.listen(8080, () => {
   console.log("hi");
 
 });
+const userSchema = new mongoose.Schema({
+  name: {type:String},
+  
+});
+const user = mongoose.model("User",userSchema);
 
+app.get("/", async(req, res) => res.send("good morning"));
+
+app.get("/register", async (req, res) => {
+  const result=await user.insertOne({name:"jonh"})
+  return res.json(result);
+});
 app.get("/greet", (req, res) => res.send("Hello World"));
+
 app.get("/weather", (req, res) => res.send("31degrees"));
 
 app.get("/products", (req, res) => {
@@ -22,35 +34,3 @@ app.get("/products", (req, res) => {
     { id: 3, name: "Headphones", price: 3000 }
   ]);
 });
-
-/*import express from "express";
-import cors from "cors";
-const app = express();
-app.listen(8080, () => {
-  console.log("Server Started on port 8080");
-});
-
-app.use(express.json());
-app.use(cors());
-const users = [];
-
-app.get("/", (req, res) => {
-  return res.json(users);
-});
-
-app.post("/register", (req, res) => {
-  const { name, email, pass } = req.body;
-  users.push({ name, email, pass });
-  return res.json(users);
-});
-
-app.post("/login", (req, res) => {
-  const { email, pass } = req.body;
-  const found = users.find(
-    (value) => value.email === email && value.pass === pass
-  );
-  if (!found) {
-    res.json({message:"User not found"})
-  }
-  res.json({...found,token:"123"});
-});*/
