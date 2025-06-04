2c9f5a9a30da8bd1725ca9e4ec2af8f5713edb17
rajyalaxmi29 <rajyalaxmikunchala06@gmail.com>
2025-06-04 15:13:42 +0530

    update

--- models/productModel.js
+++ models/productModel.js
@@ -1,8 +1,11 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+
 const productSchema = mongoose.Schema({
   name: { type: String },
   price: { type: Number },
 });
-// const product = mongoose.model("Product", productSchema);
 
 export default mongoose.model("Product", productSchema);
+
+// const product = mongoose.model("Product", productSchema);
+
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,16 @@
-import express from 'express'
-import productModel from "../models/userModel.js";
+import express from 'express';
+import Product from '../models/productModel.js';
 
-const productRouter = express.Router()
+const productRouter = express.Router();
 
+// GET /products/all
 productRouter.get("/all", async (req, res) => {
-  const products = await productModel.find();
-  res.json(products);
+  try {
+    const products = await Product.find(); // This will fetch all the products
+    res.json(products); // Send them as a JSON response
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching products", error });
+  }
 });
 
-export default productRouter
+export default productRouter;
